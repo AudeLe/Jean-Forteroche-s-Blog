f8931802b4bfd85147fd98dd947ef5ed04e8f9aa
AudeL <aude.leissen@gmail.com>
2018-03-26 18:41:54 +0200

    Initial commit allowing to edit a comment

---
+++ view/frontend/editCommentView.php
@@ -0,0 +1,26 @@
+<?php $title = htmlspecialchars('Edition de commentaire'); ?>
+
+<?php ob_start(); ?>
+	
+	<h2>Commentaires</h2>
+
+	<?php
+		$comment = $editedComment->fetch();
+	?>
+
+	<form action="index.php?action=editedComment&id=<?= $comment['id'] ?>" method="post">
+		<div>
+			<p>Auteur : <?= $comment['author'] ?></p>
+		</div>
+		<div>
+			<label for="newComment">Commentaire</label><br />
+			<textarea id="newComment" name="newComment"><?= $comment['comment'] ?></textarea>
+		</div>
+		<div>
+			<input type="submit" value="Editer le commentaire" />
+		</div>
+	</form>
+
+<?php $content = ob_get_clean(); ?>
+
+<?php require('template.php'); ?>
