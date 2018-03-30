e7c15cfafe6a570598cc3d3ab1043aee23bd125c
AudeL <aude.leissen@gmail.com>
2018-03-30 17:03:50 +0200

    Routes changed to tally the new architecture.

--- template/listPostsView.php
+++ template/listPostsView.php
@@ -10,13 +10,13 @@
 			<div class="news">
 				<h3>
 					<?= htmlspecialchars($data['title']) ?><br/ >
-					<em>le <?= $data['creation_date_fr'] ?></em><a href="home.php?action=editPost&id=<?= $data['id'] ?>"> - Editer le post</a><br />
-					<a href="home.php?action=deletePost&id=<?= $data['id'] ?>">Supprimer le post</a>
+					<em>le <?= $data['creation_date_fr'] ?></em><a href="../public/index.php?action=editPost&id=<?= $data['id'] ?>"> - Editer le post</a><br />
+					<a href="../public/index.php?action=deletePost&id=<?= $data['id'] ?>">Supprimer le post</a>
 				</h3>
 
 				<p>
 					<?= nl2br(htmlspecialchars($data['content'])) ?><br />
-					<em><a href="home.php?action=post&id=<?= $data['id'] ?>">Commentaires</a></em>
+					<em><a href="../public/index.php?action=post&id=<?= $data['id'] ?>">Commentaires</a></em>
 				</p>
 			</div>
 
