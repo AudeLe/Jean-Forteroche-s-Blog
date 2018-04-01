b1cde8021a426ba1e32adb984e5989b466a3c559
AudeL <aude.leissen@gmail.com>
2018-04-01 18:03:34 +0200

    The connection form and button are added. The JS script tag controlling the appearance and disappearance of the connection form has been added too.

--- template/template.php
+++ template/template.php
@@ -7,10 +7,27 @@
 	</head>
 
 	<body>
+
+
+        <button id ="connection">Connexion</button>
+        <form action = "../public/index.php?action=connection" id="connectionForm">
+            <div>
+                <label for="login">Identifiant</label><br />
+                <input type="text" id="login" name="login"/>
+            </div>
+            <div>
+                <label for="password">Mot de passe</label><br />
+                <input type="password" id="password" name="password"/>
+            </div>
+            <div>
+                <input type="submit" value="S'inscrire"/>
+            </div>
+        </form>
 		<p><a href="../template/addPostView.php">Ajouter un nouvel article</a></p>
 
 		<?= $content ?>
 
         <script src = "js/moderation.js"></script>
+        <script src = "js/connection.js"></script>
 	</body>
 </html>
