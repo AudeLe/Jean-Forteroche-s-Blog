b9cad28af3a8a65f32d6d87ea2129f37b75e734c
AudeL <aude.leissen@gmail.com>
2018-04-03 19:34:41 +0200

    Registration form and session tags (if set) have been added.

--- template/template.php
+++ template/template.php
@@ -1,3 +1,9 @@
+<?php
+    if(isset($_SESSION['id']) && isset($_SESSION['login'])){
+        echo 'Bonjour ' . $_SESSION['login'];
+    }
+?>
+
 <!DOCTYPE html>
 <html>
 	<head>
@@ -9,8 +15,8 @@
 	<body>
 
 
-        <button id ="connection">Connexion</button>
-        <form action = "../public/index.php?action=connection" id="connectionForm" method="post">
+        <button id ="registration">Inscription</button>
+        <form action = "../public/index.php?action=registration" id="registrationForm" method="post">
             <div>
                 <label for="login">Identifiant</label><br />
                 <input type="text" id="login" name="login"/>
@@ -23,6 +29,22 @@
                 <input type="submit" value="S'inscrire"/>
             </div>
         </form>
+
+        <button id ="connection">Connexion</button>
+        <form action = "../public/index.php?action=connection" id="connectionForm" method="post">
+            <div>
+                <label for="login">Identifiant</label><br />
+                <input type="text" id="login" name="login"/>
+            </div>
+            <div>
+                <label for="passwordVisitor">Mot de passe</label><br />
+                <input type="password" id="passwordVisitor" name="passwordVisitor"/>
+            </div>
+            <div>
+                <input type="submit" value="Se connecter"/>
+            </div>
+        </form>
+
 		<p><a href="../template/addPostView.php">Ajouter un nouvel article</a></p>
 
 		<?= $content ?>
