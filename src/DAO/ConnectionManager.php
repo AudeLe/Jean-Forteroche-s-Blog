1a161bb661294709244413cf9dcdf856f06d0df7
AudeL <aude.leissen@gmail.com>
2018-04-03 10:42:09 +0200

    The password_hash function is added.

--- src/DAO/ConnectionManager.php
+++ src/DAO/ConnectionManager.php
@@ -9,10 +9,12 @@ namespace Openclassrooms\Blog\Model;
         public function connection($login, $passwordVisitor){
             $db = $this -> dbConnect();
 
+            $passwordVisitorHashed = password_hash($passwordVisitor, PASSWORD_DEFAULT);
+
             $connectionRequest = $db -> prepare('INSERT INTO members(login, password, registration_date) VALUES (:login, :password, NOW())');
             $executeConnection = $connectionRequest -> execute(array(
                 'login' => $login,
-                'password' => $passwordVisitor
+                'password' => $passwordVisitorHashed
             ));
 
             return $executeConnection;
