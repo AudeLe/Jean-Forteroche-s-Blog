37afd40fb043534bfa74037aca2d7b217d3f2e7b
AudeL <aude.leissen@gmail.com>
2018-04-03 19:35:48 +0200

    The connection's code has been added. The password_hash() function has also been added. Trying to set in place the datas for the session.

--- src/DAO/ConnectionManager.php
+++ src/DAO/ConnectionManager.php
@@ -6,17 +6,45 @@ namespace Openclassrooms\Blog\Model;
 
     class ConnectionManager extends Manager{
 
-        public function connection($login, $passwordVisitor){
+        public function registration($login, $passwordVisitor){
             $db = $this -> dbConnect();
 
             $passwordVisitorHashed = password_hash($passwordVisitor, PASSWORD_DEFAULT);
 
-            $connectionRequest = $db -> prepare('INSERT INTO members(login, password, registration_date) VALUES (:login, :password, NOW())');
-            $executeConnection = $connectionRequest -> execute(array(
+            $registrationRequest = $db -> prepare('INSERT INTO members(login, password, registration_date) VALUES (:login, :password, NOW())');
+            $executeRegistration = $registrationRequest -> execute(array(
                 'login' => $login,
                 'password' => $passwordVisitorHashed
             ));
 
-            return $executeConnection;
+            return $executeRegistration;
+        }
+
+        public function connection($login, $passwordVisitor){
+            $db = $this -> dbConnect();
+
+            $pass_exist = $db -> prepare('SELECT id, password FROM members WHERE login = :login');
+            $pass_exist -> execute(array(
+                'login' => $login
+            ));
+
+            $resultat = $pass_exist -> fetch();
+
+            $resultat2 = password_verify($passwordVisitor, $resultat['password']);
+
+            //return $resultat2;
+
+            if ($resultat2 == false){
+                echo 'Mauvais identifiant ou mot de passe.';
+            } else {
+                if ($resultat2 == true){
+                    session_start();
+                    $_SESSION['id'] = $resultat['id'];
+                    $_SESSION['login'] = $login;
+                    echo 'Vous êtes connecté !';
+                } else {
+                    echo 'Mauvais identifiant ou mot de passe.';
+                }
+            }
         }
     }
