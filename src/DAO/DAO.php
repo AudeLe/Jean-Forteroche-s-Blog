e81391079fa08b77cf08967ecc8e6f7693238e7f
AudeL <aude.leissen@gmail.com>
2018-04-06 22:10:43 +0200

    Change of name from 'Manager' to 'DAO'.

--- src/DAO/DAO.php
+++ src/DAO/DAO.php
@@ -2,7 +2,7 @@
 
 	namespace Openclassrooms\Blog\Model;
 
-	abstract class Manager {
+	abstract class DAO {
 
 		const DB_HOST = 'mysql:host=localhost;dbname=blog;charset=utf8';
 		const DB_USER = 'root';
