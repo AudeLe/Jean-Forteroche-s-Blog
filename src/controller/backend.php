d3872afab08e479506033132bc785137d32a2977
AudeL <aude.leissen@gmail.com>
2018-04-06 22:04:41 +0200

    Add of the checkInformations and editInformations.

--- src/controller/backend.php
+++ src/controller/backend.php
@@ -1,22 +1,35 @@
 <?php
 
-    require_once('../src/DAO/ConnectionManager.php');
+    require_once('../src/DAO/ConnectionDAO.php');
 
     // Registration on the website
-    function registration($login, $passwordVisitor){
-        $connectionManager = new \Openclassrooms\Blog\Model\ConnectionManager();
+    function registration($login, $passwordVisitor, $passwordVisitorCheck){
+        $connectionManager = new \Openclassrooms\Blog\Model\ConnectionDAO();
 
-        $connectionManager -> registration($login, $passwordVisitor);
+        $connectionManager -> registration($login, $passwordVisitor, $passwordVisitorCheck);
 
         header('Location: ../public/index.php');
     }
 
     // Connection on the website
     function connection($login, $passwordVisitor){
-        $connectionManager = new \Openclassrooms\Blog\Model\ConnectionManager();
+        $connectionManager = new \Openclassrooms\Blog\Model\ConnectionDAO();
 
         $connectionManager -> connection($login, $passwordVisitor);
 
-        header('Location: ../public/index.php');
+        //header('Location: ../public/index.php');
     }
-    
+
+    function checkInformations($checkLogin, $checkPassword){
+        $connectionManager = new \Openclassrooms\Blog\Model\ConnectionDAO();
+
+        $visitorInformations = $connectionManager -> checkInformations($checkLogin, $checkPassword);
+
+        require('../template/editInformations.php');
+    }
+
+    function editInformations($idVisitor, $editLogin, $editPassword, $editPasswordCheck){
+        $connectionManager = new \Openclassrooms\Blog\Model\ConnectionDAO();
+
+        $connectionManager -> editInformations($idVisitor, $editLogin, $editPassword, $editPasswordCheck);
+    }
