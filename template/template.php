3b5bc4979df1d22b8bcbd959779a3d27798bfb2c
AudeL <aude.leissen@gmail.com>
2018-04-14 18:56:55 +0200

    Deletion of the session_start(). Adding of the Bootstrap CDN (with the scripts needed).

--- template/template.php
+++ template/template.php
@@ -1,11 +1,13 @@
 <?php
 
-    session_start();
-    session_regenerate_id(true); // Change the session id on each log in
-    if(isset($_SESSION['id']) && isset($_SESSION['login'])){
-        echo 'Bonjour ' . $_SESSION['login'];
-    }
+    //session_start();
+    //session_regenerate_id(true); // Change the session id on each log in
+    /*if(isset($_SESSION['id']) && isset($_SESSION['login'])){
+        $loginSession = $_SESSION['login'];
+        echo 'Bonjour ' . $loginSession;
+    }*/
 ?>
+
 
 <!DOCTYPE html>
 <html>
@@ -15,7 +17,9 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content = "width=device-width, initial-scale=1">
 
-        <link rel="stylesheet" href="../public/css/bootstrap-3.3.7-dist/css/bootstrap.css" />
+        <!--<link rel="stylesheet" href="../public/css/bootstrap-4.1.0/dist/css/bootstrap.css" />-->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+
 		<link rel="stylesheet" href="../public/css/style.css" />
 
         <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
@@ -68,6 +72,11 @@
         <a href="../template/logOut.php">Déconnexion</a>
 
 		<?= $content ?>
+
+        <!--<script src = "css/bootstrap-4.1.0/dist/js/bootstrap.js"></script>-->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
 
         <script src = "js/moderation.js"></script>
         <script src = "js/connection.js"></script>
