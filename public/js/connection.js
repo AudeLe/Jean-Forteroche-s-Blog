7dddf2097adc8e0cbb8e60dd187ebe6263ac1e69
AudeL <aude.leissen@gmail.com>
2018-04-21 00:55:56 +0200

    Deletion of un-necessary functions.

--- public/js/connection.js
+++ public/js/connection.js
@@ -1,29 +1,12 @@
 var connection = {
 
-    init: function(){
+    init: function () {
         registrationBtn = document.getElementById("registration");
         registrationForm = document.getElementById("registrationForm");
         connectionBtn = document.getElementById("connection");
         connectionForm = document.getElementById("connectionForm");
-    },
-
-    activatedRegistrationForm: function(){
-        connection.init();
-
-        registrationBtn.addEventListener("click", function(){
-            registrationForm.style.display = "block";
-        });
-    },
-
-    activatedConnectionForm: function(){
-        connection.init();
-
-        connectionBtn.addEventListener("click", function(){
-            connectionForm.style.display = "block";
-        });
     }
-
 }
 
-connection.activatedRegistrationForm();
-connection.activatedConnectionForm();
+
+
