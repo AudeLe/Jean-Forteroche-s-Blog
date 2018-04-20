aa12ed48facd7916c23bc059990d4cdc803a579e
AudeL <aude.leissen@gmail.com>
2018-04-21 01:06:11 +0200

    Adding the re-direction to the main page after a few seconds.

--- template/logOut.php
+++ template/logOut.php
@@ -1,10 +1,11 @@
+<?php header('refresh:5; url=../public/index.php'); ?>
+
 <?php $title = htmlspecialchars('Déconnexion'); ?>
 
 <?php ob_start(); ?>
 
     <p>Vous venez d'être déconnecté avec succès.</p>
 
-    <a href="../public/index.php">Retourner sur la page d'accueil du site</a>
 <?php $content = ob_get_clean(); ?>
 
 <?php require('template.php'); ?>
