523026a995863d618a04c2410124d6ac594870bc
AudeL <aude.leissen@gmail.com>
2018-04-23 00:35:44 +0200

    Modification of the content of the file in order to only display the informations and not execute operations.

--- template/listPostsView.php
+++ template/listPostsView.php
@@ -1,35 +1,31 @@
-<?php $title = 'Mon blog'; ?>
+<?php $this->title = 'Accueil'; ?>
 
-<?php ob_start(); ?>
 	<h1>Bienvenue sur mon blog</h1>
 
     <p>
         Je souhaite vous faire partager mon ouvrage : "Billet simple pour l'Alaska".
         Les chapitres seront publiés au fur et à mesure de leur écriture. N'hésitez pas à me faire part de vos remarques et de vos corrections.
     </p>
-	<p>Derniers chapitres :</p>
 
+
+    <div id="authorPresentation">
+        <h4>Jean Forteroche</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam mauris tortor, euismod a augue in, lacinia semper ante. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse sed quam tempus risus rutrum ullamcorper quis in eros. Nulla neque dui, eleifend ultricies dolor eu, molestie accumsan nisl. Fusce congue risus et turpis finibus fermentum ut gravida tortor. Nunc ut consectetur nunc, ut lobortis mi. Mauris venenatis vulputate turpis quis sagittis. Vestibulum lacinia ex dolor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. </p>
+    </div>
+
+    <p>Derniers chapitres :</p>
 	<?php
-		while($data = $posts->fetch()){
-			?>
-			<div class="news">
-				<h3>
-					<?= htmlspecialchars($data['title']) ?><br/ >
-					<em>le <?= $data['creation_date_fr'] ?></em>
 
-				</h3>
+        foreach($posts as $post){
+            ?>
+            <div class="news">
+                <h3><a href="../public/index.php?action=post&id=<?= $post->getId(); ?>"><?= $post->getTitle(); ?></a></h3>
+                <p><?= $post->getCreationDate(); ?></p>
+                <p><?= $post->getContent(); ?></p>
 
-				<p>
-					<?= nl2br(htmlspecialchars($data['content'])) ?><br />
-					<em><a href="../public/index.php?action=post&id=<?= $data['id'] ?>">Commentaires</a></em>
-				</p>
-			</div>
+            </div>
+            <br>
 
-			<?php
-		}
-		$posts->closeCursor();
-	?>
-
-<?php $content = ob_get_clean(); ?>
-
-<?php require('template.php'); ?>
+        <?php
+        }
+        ?>
