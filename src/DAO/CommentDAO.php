71c6e8c1d77cb0bf69fcb51cef0b8097e68a8a47
AudeL <aude.leissen@gmail.com>
2018-04-23 00:32:02 +0200

    Adding of the Comment.php. Improvement of the code in order to provide more protection to the database connection. Adding of the pagination for the posts/chapters.

--- src/DAO/CommentDAO.php
+++ src/DAO/CommentDAO.php
@@ -2,14 +2,56 @@
 
 namespace Blog\src\DAO;
 
-require_once('DAO.php');
+use Blog\src\classes\Comment;
+
+//require_once('DAO.php');
 
 class CommentDAO extends DAO{
 
 	public function getComments($postId){
-		$db = $this -> dbConnect();
-		$comments = $db -> prepare('SELECT id, author, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%imin%ss\') AS comment_date_fr FROM comments WHERE post_id = ? ORDER BY comment_date DESC');
-		$comments -> execute(array($postId));
+
+	    $sql = 'SELECT COUNT(id) as nbComments FROM comments WHERE post_id = ?';
+	    $result = $this->sql($sql, [$postId]);
+        $countCommentsData = $result -> fetch();
+
+		//$db = $this -> dbConnect();
+
+        //$countComments = $db -> prepare('SELECT COUNT(id) as nbComments FROM comments WHERE post_id = :post_id');
+        //$countComments -> execute(array(
+            //'post_id' => $postId
+        //));
+        //$countCommentsData = $countComments -> fetch();
+
+        $nbComments = $countCommentsData['nbComments'];
+        $perPage = 5;
+        $nbPage = ceil($nbComments/$perPage);
+
+        if(isset($_GET['p']) && $_GET['p'] > 0 && $_GET['p'] <= $nbPage){
+            $cPage = $_GET['p'];
+        } else {
+            $cPage= 1;
+        }
+
+        $sql = 'SELECT id, post_id, author, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%imin%ss\') AS comment_date_fr FROM comments WHERE post_id = ? ORDER BY comment_date DESC LIMIT '.(($cPage-1)*$perPage).','.$perPage.'';
+        $result = $this->sql($sql, [$postId]);
+        $comments = [];
+
+        foreach ($result as $row){
+            $commentId = $row['id'];
+            $comments[$commentId] = $this->buildObject($row);
+        }
+
+		//$comments = $db -> prepare('SELECT id, author, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%imin%ss\') AS comment_date_fr FROM comments WHERE post_id = ? ORDER BY comment_date DESC LIMIT '.(($cPage-1)*$perPage).','.$perPage.'');
+		//$comments -> execute(array($postId));
+
+        for($i = 1; $i <= $nbPage; $i++){
+            if($i == $cPage){
+                echo "$i /";
+            } else {
+                echo "<a href =\"../public/index.php?action=post&id=$postId&p=$i\">$i</a>";
+            }
+        }
+
 
 		return $comments;
 	}
@@ -48,4 +90,14 @@ class CommentDAO extends DAO{
 			'id' => $id
 		));
 	}
+
+	private function buildObject(array $row){
+	    $comment = new Comment();
+	    $comment->setId($row['id']);
+	    $comment->setPostIt($row['post_id']);
+        $comment->setAuthor(($row['author']));
+        $comment->setComment($row['comment']);
+        $comment->setCommentDate($row['comment_date_fr']);
+        return $comment;
+    }
 }
