0520e937de5cde4d66206a5f7b1155c294774ab6
AudeL <aude.leissen@gmail.com>
2018-04-25 00:08:21 +0200

    Message to confirm the registration or to explain that the login is already taken.

---
+++ template/registration.php
@@ -0,0 +1,9 @@
+<?php header('refresh:5; url=../public/index.php'); ?>
+
+<?php $this->title = htmlspecialchars('Inscription'); ?>
+
+
+<p>
+Vous allez être redirigé vers la page d'accueil du blog.<br />
+    Si ce n'est pas le cas, veuillez cliquer sur ce <a href="../public/index.php">lien</a>.
+</p>
