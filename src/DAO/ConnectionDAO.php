2f783f3e25e075736bd79873e0eefea6e9689ae1
AudeL <aude.leissen@gmail.com>
2018-04-25 00:01:22 +0200

    Deletion of the require. Adding the 'use' to access the Post and Comment classes. Improvement of the database and request code. Adding 2 functions to build objects.

--- src/DAO/ConnectionDAO.php
+++ src/DAO/ConnectionDAO.php
@@ -1,126 +1,111 @@
 <?php
 
-namespace Blog\src\DAO;
+    namespace Blog\src\DAO;
 
-    require_once('DAO.php');
+    use Blog\src\classes\Post;
+    use Blog\src\classes\Comment;
 
     class ConnectionDAO extends DAO{
 
-        public function registration($login, $passwordVisitor, $passwordVisitorCheck){
-            $db = $this -> dbConnect();
+        public function registration($login, $passwordVisitor, $passwordVisitorCheck)
+        {
 
-            $loginExists = $db -> prepare('SELECT login FROM members WHERE login = :login');
-            $loginExists -> execute(array(
-                'login' => $login
-            ));
+            $sql = 'SELECT login FROM members WHERE login = ?';
+            $result = $this->sql($sql, [$login]);
+            $row = $result->fetch();
 
-            if($donnees = $loginExists -> fetch()){
+            if ($row) {
                 echo 'Ce pseudo est déjà utilisé.';
             } else {
-                if ($passwordVisitor == $passwordVisitorCheck){
+                if ($passwordVisitor === $passwordVisitorCheck) {
 
                     $passwordVisitorHashed = password_hash($passwordVisitor, PASSWORD_DEFAULT);
 
-                    $registrationRequest = $db -> prepare('INSERT INTO members(login, password, registration_date) VALUES (:login, :password, NOW())');
-                    $executeRegistration = $registrationRequest -> execute(array(
+                    $sql = 'INSERT INTO members(login, password, registration_date) VALUES (:login, :password, NOW())';
+                    $result = $this->sql($sql, [
                         'login' => $login,
                         'password' => $passwordVisitorHashed
-                    ));
+                    ]);
 
-                    return $executeRegistration;
-
+                    echo 'Votre inscription a bien été prise en compte. Veuillez vous connecter afin d\'accéder à votre espace personnel.';
                 } else {
                     echo 'Vous n\'avez pas saisi les mêmes mots de passe.';
                 }
             }
+
         }
 
         public function connection($login, $passwordVisitor){
-            $db = $this -> dbConnect();
 
-            $pass_exist = $db -> prepare('SELECT id, password, status FROM members WHERE login = :login');
-            $pass_exist -> execute(array(
-                'login' => $login
-            ));
+            $sql = 'SELECT id, password, status FROM members WHERE login = ?';
+            $result = $this->sql($sql, [$login]);
+            $row = $result->fetch();
+            if($row){
+                $checkPassword = password_verify($passwordVisitor, $row['password']);
+                if($checkPassword == true){
+                    //Charging the informations of the session
 
-            $connectionDetails = $pass_exist -> fetch();
-
-            $result2 = password_verify($passwordVisitor, $connectionDetails['password']);
-
-            if ($result2 == false){
-                echo 'Mauvais identifiant ou mot de passe.';
-            } else {
-                if ($result2 == true){
-                    session_start();
-                    $_SESSION['id'] = $connectionDetails['id'];
+                    $_SESSION['id'] = $row['id'];
                     $_SESSION['login'] = $login;
+                    $_SESSION['status'] = $row['status'];
+
                     echo 'Vous êtes connecté !';
-                    var_dump($connectionDetails['status']);
-                    //die();
-                    if($connectionDetails['status'] == 'admin'){
-                        var_dump($connectionDetails['id']);
-                        //die();
 
-                        header('Location: ../template/adminView.php');
-
-                        return $connectionDetails;
-                    }else{
-                        var_dump($connectionDetails['id']);
-                        //die();
-                        header('Location: ../template/memberView.php');
-
-                        return $connectionDetails;
+                    if ($row['status'] == 'admin'){
+                        $this->getChapters();
+                        header('Location: ../public/index.php?action=getChaptersAndReportedComments');
+                    } else {
+                        header('Location: ../public/index.php?action=getMemberComments&login='.$login.'');
                     }
-
                 } else {
-                    echo 'Mauvais identifiant ou mot de passe.';
+                    echo 'Mauvais identifiant ou mot de passe';
                 }
+            } else {
+                echo 'Mauvais identifiant ou mot de passe';
             }
 
+        }
+
+        public function logOut(){
+            $_SESSION = array();
+            session_destroy();
 
         }
 
         public function checkInformations($checkLogin, $checkPassword){
-            $db = $this->dbConnect();
 
-            $req = $db -> prepare('SELECT id, password FROM members WHERE login = :login');
-            $req -> execute(array(
+
+            $sql = 'SELECT id, password FROM members WHERE login = :login';
+            $result = $this->sql($sql, [
                 'login' => $checkLogin
-            ));
+            ]);
+            $row = $result->fetch();
+            if($row){
+                $confirmPassword = password_verify($checkPassword, $row['password']);
 
-            $visitorInformations = $req -> fetch();
-
-            $visitorPassword = password_verify($checkPassword, $visitorInformations['password']);
-
-            // Add a condition => if $checkLogin and $checkPassword = Session's Login and Password = true
-            if($visitorPassword == true){
-                echo 'Vous avez entré les bons identifiants.';
-                //die();
-            } else {
-                echo 'Mauvais identifiant ou mot de passe.';
+                if($confirmPassword == true){
+                    echo 'Vous avez entré les bons identifiants.';
+                } else {
+                    echo 'Mauvais identifiant ou mot de passe';
+                }
             }
 
-            return $visitorInformations;
         }
 
         public function editPassword($idVisitor, $editPassword, $editPasswordCheck){
-            $db = $this -> dbConnect();
 
             if($editPassword == $editPasswordCheck){
                 $editPasswordHashed = password_hash($editPassword, PASSWORD_DEFAULT);
 
-                $req = $db -> prepare('UPDATE members SET password = :password WHERE id = :id');
-                $req -> execute(array(
+                $sql = 'UPDATE members SET password = :password WHERE id = :id';
+                $this->sql($sql, [
                     'id' => $idVisitor,
                     'password' => $editPasswordHashed
-                ));
-
-                header('Location: ../public/index.php');
+                ]);
 
                 echo 'Mot de passe modifié.';
 
-                //self::checkStatus($req);
-
+                header('Location: ../public/index.php');
             } else {
                 echo 'Votre mot de passe n\'a pas pu être modifié.';
             }
@@ -128,66 +113,69 @@ namespace Blog\src\DAO;
         }
 
         public function editLogin($idVisitor, $editLogin){
-            $db = $this->dbConnect();
 
-            $loginExists = $db -> prepare('SELECT login FROM members WHERE login = :login');
-            $loginExists -> execute(array(
+            $sql = 'SELECT login FROM members WHERE login = :login';
+            $result = $this->sql($sql, [
                 'login' => $editLogin
-            ));
-
-            if($donnees = $loginExists -> fetch()){
+            ]);
+            $row = $result->fetch();
+            if($row){
                 echo 'Ce pseudo est déjà utilisé.';
             } else {
-                $updateLogin = $db -> prepare('UPDATE members SET login = :login WHERE id = :id');
-                $updateLogin -> execute(array(
+                $sql = 'UPDATE members SET login = :login WHERE id = :id';
+                $result = $this->sql($sql, [
                     'login' => $editLogin,
                     'id' => $idVisitor
-                ));
+                ]);
 
-                echo 'Pseudo modifié.';
+                echo 'Login modifié';
             }
         }
 
-        public function getPosts(){
-            $db = $this -> dbConnect();
-            $req = $db -> query('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts ORDER BY creation_date DESC LIMIT 0, 5');
+        public function getChapters(){
 
-            /*$posts = array();
-            foreach($req as $row){
+            $sql = 'SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts ORDER BY creation_date DESC LIMIT 0, 5';
+            $result = $this->sql($sql);
+            $posts = [];
+            foreach($result as $row){
                 $postId = $row['id'];
-                $posts[$postId] = $this-> buildClassObject($row);
-            }*/
+                $posts[$postId] = $this->buildObject($row);
+            }
+            return $posts;
 
-            return $req;
-        }
-
-        public function getMemberComments($login){
-            $db = $this ->dbConnect();
-            $memberComments = $db -> prepare('SELECT id, post_id, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%imin%ss\') AS comment_date_fr FROM comments WHERE author = ? ORDER BY comment_date DESC LIMIT 0, 5');
-            $memberComments -> execute(array($login));
-
-            //$memberComments = $req -> fetch();
-
-            //var_dump($memberComments);
-            //die();
-            return $memberComments;
-        }
-
-        public function reportComment($idComment){
-            $db = $this -> dbConnect();
-            $reportedComment = $db -> prepare('UPDATE comments SET reported = 1 WHERE id = ?');
-            $reportedComment -> execute(array($idComment));
-
-            var_dump($idComment);
-            //die();
-            return $reportedComment;
         }
 
         public function getReportedComments(){
-            $db = $this -> dbConnect();
-            $reportedVisitorComments = $db -> query('SELECT id, post_id, author, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%imin%ss\') AS comment_date_fr FROM comments WHERE reported = 1 ORDER BY comment_date DESC LIMIT 0,5');
 
-            //var_dump($reportedVisitorComments);
-            return $reportedVisitorComments;
+            $sql = 'SELECT id, post_id, author, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%imin%ss\') AS comment_date_fr FROM comments WHERE reported = 1 ORDER BY comment_date DESC LIMIT 0,5';
+            $result = $this->sql($sql);
+
+            $comments = [];
+            foreach($result as $row){
+                $commentId = $row['id'];
+                $comments[$commentId] = $this->buildObjectComment($row);
+            }
+            return $comments;
+
+        }
+
+
+        private function buildObject(array $row){
+            $post = new Post();
+            $post->setId($row['id']);
+            $post->setTitle($row['title']);
+            $post->setContent($row['content']);
+            $post->setCreationDate($row['creation_date_fr']);
+            return $post;
+        }
+
+        private function buildObjectComment(array $row){
+            $comment = new Comment();
+            $comment->setId($row['id']);
+            $comment->setPostId($row['post_id']);
+            $comment->setAuthor(($row['author']));
+            $comment->setComment($row['comment']);
+            $comment->setCommentDate($row['comment_date_fr']);
+            return $comment;
         }
     }
