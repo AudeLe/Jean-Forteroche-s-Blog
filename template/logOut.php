84c9aa8b20ab66dc7aa472a64170a6da8d5f81f4
AudeL <aude.leissen@gmail.com>
2018-04-25 00:23:12 +0200

    Deletion of the ob_start(), the ob_get_clean() and of the session_destroy().

--- template/logOut.php
+++ template/logOut.php
@@ -1,13 +1,8 @@
 <?php header('refresh:5; url=../public/index.php'); ?>
 
-<?php $title = htmlspecialchars('Déconnexion'); ?>
+<?php $this->title = htmlspecialchars('Déconnexion'); ?>
 
-<?php ob_start(); ?>
+
 
     <p>Vous venez d'être déconnecté avec succès.</p>
 
-<?php $content = ob_get_clean(); ?>
-
-<?php require('template.php'); ?>
-
-<?php session_destroy(); ?>
