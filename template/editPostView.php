5b37f18120ce49d3fcdcedf2c5db1b9980b351ae
AudeL <aude.leissen@gmail.com>
2018-04-25 00:21:59 +0200

    Modification of the way the post informations are displayed.

--- template/editPostView.php
+++ template/editPostView.php
@@ -1,27 +1,19 @@
-<?php $title = htmlspecialchars('Edition de post'); ?>
+<?php $this->title = 'Edition de post'; ?>
 
-<?php ob_start(); ?>
-	<h2>Post</h2>
+	<h2>Edition de chapitre</h2>
 
-	<?php
-		$post = $editedPost->fetch();
-	?>
 
-	<form action="../public/index.php?action=editedPost&id=<?= $post['id']?>" method="post">
+	<form action="../public/index.php?action=editedPost&id=<?= $post->getId(); ?>" method="post">
 		<div>
 			<label for="newTitle">Titre du post</label><br />
-			<textarea id="newTitle" name="newTitle"><?= $post['title'] ?></textarea>
+			<textarea id="newTitle" name="newTitle"><?= $post->getTitle(); ?></textarea>
 		</div>
 		<div>
 			<label for="newPost">Post</label><br />
-			<textarea id="newPost" name="newPost"><?= $post['content'] ?></textarea>
+			<textarea id="newPost" name="newPost"><?= $post->getContent(); ?></textarea>
 		</div>
 		<div>
 			<input type="submit" value="Editer le post" />
 		</div>
 		
 	</form>
-
-<?php $content = ob_get_clean(); ?>
-
-<?php require ('template.php'); ?>
