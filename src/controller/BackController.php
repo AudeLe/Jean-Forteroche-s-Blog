67e7de8ccb76b298d10d9d9ad8eea3bf39498b86
AudeL <aude.leissen@gmail.com>
2018-04-24 23:52:27 +0200

    Adding the View class. Modification of the getPosts() and getComments(). They have been merged together. Adding a view on the registration function. Adding the logOut function. Modification of the reportComment() function's site.

--- src/controller/BackController.php
+++ src/controller/BackController.php
@@ -3,8 +3,7 @@
     namespace Blog\src\controller;
 
     use Blog\src\DAO\ConnectionDAO;
-
-    //require_once('../src/DAO/ConnectionDAO.php');
+    use Blog\src\Classes\View;
 
     class BackController{
 
@@ -13,41 +12,45 @@
             $this->connectionManager = new ConnectionDAO();
         }
 
-        public function getPosts(){
-            //$connectionManager = new \Openclassrooms\Blog\Model\ConnectionDAO();
-            $postsAdmin = $this->connectionManager->getPosts();
-            $reportedVisitorComments = $this->connectionManager->getReportedComments();
+        public function getChaptersAndReportedComments(){
 
-            require('../template/adminView.php');
-        }
+            $posts = $this->connectionManager->getChapters();
+            $comments = $this->connectionManager->getReportedComments();
 
-        public function getComments($loginSession){
-            //$getCommentsManager = new \Openclassrooms\Blog\Model\ConnectionDAO();
-            $memberComments = $this->connectionManager->getMemberComments($loginSession);
 
-            require('../template/memberView.php');
+            $view = new View('adminView');
+            $view->render([
+                'posts' => $posts,
+                'comments' => $comments
+            ]);
+
         }
 
         // Registration on the website
         public function registration($login, $passwordVisitor, $passwordVisitorCheck){
-            //$connectionManager = new \Openclassrooms\Blog\Model\ConnectionDAO();
 
             $this->connectionManager->registration($login, $passwordVisitor, $passwordVisitorCheck);
 
-            header('Location: ../public/index.php');
+            $view = new View('registration');
+            $view->render([]);
         }
 
         // Connection on the website
         public function connection($login, $passwordVisitor){
-            //$connectionManager = new \Openclassrooms\Blog\Model\ConnectionDAO();
 
             $connectionDetails = $this->connectionManager->connection($login, $passwordVisitor);
-            $memberComments = $this->connectionManager->getMemberComments($login);
-            //header('Location: ../public/index.php');
+
+        }
+
+
+        public function logOut(){
+            $this->connectionManager->logOut();
+
+            $view = new View('logOut');
+            $view->render([]);
         }
 
         public function checkInformations($checkLogin, $checkPassword){
-            //$connectionManager = new \Openclassrooms\Blog\Model\ConnectionDAO();
 
             $visitorInformations = $this->connectionManager->checkInformations($checkLogin, $checkPassword);
 
@@ -64,21 +67,6 @@
 
             $this->connectionManager->editLogin($idVisitor, $editLogin);
 
-        }
-
-        public function reportComment($idComment){
-            //$connectionManager = new \Openclassrooms\Blog\Model\ConnectionDAO();
-
-            $reportedComment = $this->connectionManager->reportComment($idComment);
-
-            //require('../template/adminView.php');
-        }
-
-        public function getReportedComments(){
-            //$connectionManager = new \Openclassrooms\Blog\Model\ConnectionDAO();
-            $reportedVisitorComments = $this->connectionManager->getReportedComments();
-
-            require('../template/adminView.php');
         }
 
     }
