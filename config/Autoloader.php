ccf535068ec871e41c32558ee71a5215fac2bd99
AudeL <aude.leissen@gmail.com>
2018-04-26 18:05:13 +0200

    Adding the PHP documentation.

--- config/Autoloader.php
+++ config/Autoloader.php
@@ -4,10 +4,16 @@
 
 	class Autoloader {
 
+        /**
+         *
+         */
 		public static function register(){
 			spl_autoload_register([__CLASS__, 'autoload']);
 		}
 
+        /**
+         * @param $class
+         */
 		public static function autoload($class){
 			$class = str_replace('Blog', '', $class);
 			$class = str_replace('\\', '/', $class);
