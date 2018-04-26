bcabe53475b84257d627e578c7705f5eb7b191ba
AudeL <aude.leissen@gmail.com>
2018-04-26 18:03:42 +0200

    Adding the PHP documentation and some comments on the code.

--- config/Router.php
+++ config/Router.php
@@ -10,21 +10,31 @@
         private $frontController;
         private $backController;
 
+        /**
+         * Router constructor.
+         */
         public function __construct(){
             $this->frontController = new FrontController();
 
             $this->backController = new BackController();
         }
 
+        /**
+         *
+         */
         public function requestRouter(){
 
             try{
+
                 if(isset($_GET['action'])){
+                    // Lists of chapters - Home page - Default action
                     if($_GET['action'] == 'listPosts'){
                         $this->frontController->listPosts();
-                        //$this->frontController->paginationChapters();
                     }
 
+                                /* ----- POST/CHAPTER ----- */
+
+                    // Specific post displayed
                     elseif($_GET['action'] == 'post') {
                         if (isset($_GET['id']) && $_GET['id'] > 0) {
                             $this->frontController->post($_GET['id']);
@@ -33,19 +43,7 @@
                         }
                     }
 
-                    elseif($_GET['action'] == 'getChaptersAndReportedComments'){
-                        $this->backController->getChaptersAndReportedComments();
-                    }
-
-                    elseif($_GET['action'] == 'getMemberComments'){
-                        if(isset($_GET['login'])){
-                            $this->frontController->getMemberComments($_GET['login']);
-                        } else {
-                            throw new \Exception('Veuillez vous connecter afin d\'accéder à vos commentaires.');
-                        }
-                    }
-
-                    // Ajout d'un article
+                    // Add a chapter
                     elseif($_GET['action'] == 'addPost'){
                         if(!empty($_POST['title']) && !empty($_POST['article'])){
                             $this->frontController->addPost($_POST['title'], $_POST['article']);
@@ -54,8 +52,60 @@
                         }
                     }
 
+                    // Recovering of the post the admin want to edit
+                    elseif($_GET['action'] == 'editPost'){
+                        if(isset($_GET['id']) && $_GET['id'] > 0){
+                            $this->frontController->editPost($_GET['id']);
+                        } else{
+                            throw new \Exception('Impossible de récupérer le post.');
+                        }
+                    }
+
+                    // Recording of the modification done on the chapter into the database
+                    elseif($_GET['action'] == 'editedPost'){
+                        if(isset($_GET['id']) && $_GET['id'] > 0){
+                            $this->frontController->editedPost($_GET['id'], $_POST['newTitle'], $_POST['newPost']);
+                        } else {
+                            throw new \Exception('Aucune modification effectuée sur le post.');
+                        }
+                    }
+
+                    // Deletion of a post
+                    elseif($_GET['action'] == 'deletePost'){
+                        if(isset($_GET['id']) && $_GET['id'] > 0){
+                            $this->frontController->deletePost($_GET['id']);
+                        } else {
+                            throw new \Exception('Impossible de supprimer ce post.');
+                        }
+                    }
+
+
+                                /* ----- ADMIN PAGE ----- */
+
+                    // Return all the chapters and all the reported comments on the admin page
+                    elseif($_GET['action'] == 'getChaptersAndReportedComments'){
+                        $this->backController->getChaptersAndReportedComments();
+                    }
+
+
+                                /* ----- MEMBER PAGE ----- */
+
+                    // Return all the comments of a member when he/she is connected
+                    elseif($_GET['action'] == 'getMemberComments'){
+                        if(isset($_GET['login'])){
+                            $this->frontController->getMemberComments($_GET['login']);
+                        } else {
+                            throw new \Exception('Veuillez vous connecter afin d\'accéder à vos commentaires.');
+                        }
+                    }
+
+
+                                /* ----- COMMENT ----- */
+
+                    // Add a comment
                     elseif($_GET['action'] == 'addComment'){
                         if(isset($_GET['id']) && $_GET['id'] > 0){
+                            // Veirfy if the inputs are not empty
                             if(!empty($_POST['author']) && !empty($_POST['comment'])){
                                 $this->frontController->addComment($_GET['id'], $_POST['author'], $_POST['comment']);
                             } else {
@@ -66,6 +116,7 @@
                         }
                     }
 
+                    // Recovering of the comment we want to edit
                     elseif($_GET['action'] == 'editComment'){
                         if(isset($_GET['id']) && $_GET['id'] > 0){
                             $this->frontController->editComment($_GET['id']);
@@ -74,14 +125,20 @@
                         }
                     }
 
+                    // Modification on the database of the selected comment
                     elseif($_GET['action'] == 'editedComment'){
                         if(isset($_GET['id']) && $_GET['id'] > 0){
-                            $this->frontController->editedComment($_GET['id'], $_POST['newComment']);
+                            if(isset($_GET['memberLogin'])){
+                                $this->frontController->editedComment($_GET['id'], $_GET['memberLogin'], $_POST['newComment']);
+                            } else {
+                                throw new \Exception('Impossible d\'identifier l\'auteur du commentaire.');
+                            }
                         } else {
                             throw new \Exception('Aucune modification effectuée sur le commentaire');
                         }
                     }
 
+                    // Deletion of a comment
                     elseif($_GET['action'] == 'deleteComment'){
                         if(isset($_GET['id']) && $_GET['id'] > 0){
                             $this->frontController->deleteComment($_GET['id']);
@@ -90,6 +147,7 @@
                         }
                     }
 
+                    // Report a comment
                     elseif($_GET['action'] == 'reportComment'){
                         if(isset($_GET['id']) && $_GET['id'] > 0 && ($_GET['postId'] && $_GET['postId'] > 0)){
                             $this->frontController->reportComment($_GET['id'], $_GET['postId']);
@@ -98,29 +156,8 @@
                         }
                     }
 
-                    elseif($_GET['action'] == 'editPost'){
-                        if(isset($_GET['id']) && $_GET['id'] > 0){
-                            $this->frontController->editPost($_GET['id']);
-                        } else{
-                            throw new \Exception('Impossible de récupérer le post.');
-                        }
-                    }
 
-                    elseif($_GET['action'] == 'editedPost'){
-                        if(isset($_GET['id']) && $_GET['id'] > 0){
-                            $this->frontController->editedPost($_GET['id'], $_POST['newTitle'], $_POST['newPost']);
-                        } else {
-                            throw new \Exception('Aucune modification effectuée sur le post.');
-                        }
-                    }
-
-                    elseif($_GET['action'] == 'deletePost'){
-                        if(isset($_GET['id']) && $_GET['id'] > 0){
-                            $this->frontController->deletePost($_GET['id']);
-                        } else {
-                            throw new \Exception('Impossible de supprimer ce post.');
-                        }
-                    }
+                                /* ----- CONNECTION RELATED ----- */
 
                     // Registration on the website
                     elseif($_GET['action'] == 'registration'){
@@ -140,10 +177,12 @@
                         }
                     }
 
+                    // Log out of the website
                     elseif($_GET['action'] == 'logOut'){
                         $this->backController->logOut();
                     }
 
+                    // Checking the person's credentials before allowing the person connected to change them
                     elseif($_GET['action'] == 'checkInformations'){
                         if(!empty($_POST['checkLogin']) && !empty($_POST['checkPassword'])){
                             $this->backController->checkInformations($_POST['checkLogin'], $_POST['checkPassword']);
@@ -152,6 +191,7 @@
                         }
                     }
 
+                    // Change the password
                     elseif($_GET['action'] == 'editPassword'){
                         if(!empty($_POST['idVisitor']) && !empty($_POST['editPassword']) && !empty($_POST['editPasswordCheck'])){
                             $this->backController->editPassword($_POST['idVisitor'], $_POST['editPassword'], $_POST['editPasswordCheck']);
@@ -160,6 +200,7 @@
                         }
                     }
 
+                    // CHange the login
                     elseif($_GET['action'] == 'editLogin'){
                         if(!empty($_POST['idVisitor']) && !empty($_POST['editLogin'])){
                             $this->backController->editLogin($_POST['idVisitor'], $_POST['editLogin']);
@@ -168,6 +209,7 @@
                         }
                     }
 
+                    // Deletion of the account
                     elseif($_GET['action'] == 'deletionAccount'){
                         if(isset($_GET['id']) && $_GET['id'] > 0){
                             $this->backController->deletionAccount($_GET['id']);
@@ -178,6 +220,7 @@
                     }
 
                 } else {
+                    // Default action. Display all the chapters
                     $this->frontController->listPosts();
                 }
             } catch(\Exception $e){
