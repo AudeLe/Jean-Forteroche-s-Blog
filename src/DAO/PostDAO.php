cc692047b9cbd8995b378484c7aea4db113201ab
AudeL <aude.leissen@gmail.com>
2018-04-26 19:30:53 +0200

    Adding the PHP documentation and some comments.

--- src/DAO/PostDAO.php
+++ src/DAO/PostDAO.php
@@ -6,23 +6,29 @@
 
 	class PostDAO extends DAO{
 
-		public function getPosts(){
+        /**
+         * @return array
+         */
+	    public function getPosts(){
 
+	        // Count how many posts/chapters exists
             $sql = 'SELECT COUNT(id) as nbChapters FROM posts';
-
 		    $result = $this->sql($sql);
 		    $countPostsData = $result -> fetch();
 
+		    // Define the variables needed to establish the pagination
             $nbChapters = $countPostsData['nbChapters'];
             $perPage = 5;
             $nbPage = ceil($nbChapters/$perPage);
 
+            // Define which page of posts/chapters we are on
             if(isset($_GET['p']) && $_GET['p'] > 0 && $_GET['p'] <= $nbPage){
                 $cPage = $_GET['p'];
             } else {
                 $cPage= 1;
             }
 
+            // Recover all the posts/chapters
 		    $sql = 'SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts ORDER BY creation_date DESC LIMIT '.(($cPage-1)*$perPage).','.$perPage.'';
 		    $result = $this->sql($sql);
 		    $posts = [];
@@ -32,16 +38,20 @@
 		        $posts[$postId] = $this -> buildObject($row);
             }
 
+            // Return the pages needed
             $page = [];
-
 		    for ($i = 1; $i <= $nbPage; $i++){
 		        $page[$i] = '<a href ="../public/index.php?action=listPosts&p='.$i.'">'.$i.'</a>';
             }
 
             return [$posts, $page];
-
 		}
 
+        /**
+         * @param $id
+         * @return Post
+         */
+        // Select and return a specific post/chapters
 		public function getPost($id){
 
 		    $sql = 'SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts WHERE id = ?';
@@ -55,6 +65,10 @@
             }
 		}
 
+        /**
+         * @return array
+         */
+        // Return the list of the posts/chapters without a limitation of posts/chapters per pages
 		public function getPostsInsert(){
 
             $sql = 'SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts ORDER BY creation_date DESC';
@@ -69,6 +83,11 @@
             return $posts;
         }
 
+        /**
+         * @param $title
+         * @param $article
+         */
+        // Allows to add a post/chapter
 		public function addPost($title, $article){
 
 		    $sql = 'INSERT INTO posts(title, content, creation_date) VALUES (:title, :content, NOW())';
@@ -80,6 +99,11 @@
             header('Location: ../public/index.php?action=getChaptersAndReportedComments');
 		}
 
+        /**
+         * @param $id
+         * @return Post
+         */
+        // Recover the post/chapter we want to édit
 		public function editPost($id){
 
 		    $sql = 'SELECT id, title, content, DATE_FORMAT(creation_date, \'%d%m%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts WHERE id = ?';
@@ -92,6 +116,12 @@
             }
 		}
 
+        /**
+         * @param $id
+         * @param $newTitle
+         * @param $newPost
+         */
+        // Recording of the changes made on the post/chapter
 		public function editedPost($id, $newTitle, $newPost){
 
 		    $sql = 'UPDATE posts SET title = :newTitle, content = :newPost WHERE id = :id';
@@ -102,13 +132,19 @@
             ]);
 		}
 
-
+        /**
+         * @param $id
+         */
+        // Allows to delete a post/chapter and the comments tied to it
 		public function deletePost($id){
 
 		    $sql = 'SELECT comment FROM comments WHERE post_id = ?';
 		    $result = $this->sql($sql, [$id]);
 		    $row = $result -> fetch();
 
+		    // Check if they are comments tied to the post/chapter selected
+            // if yes, delete both the post/chapter and the comments
+            // otherwise delete only the post/chapter
 		    if($row){
                 $sql = 'DELETE posts, comments FROM posts INNER JOIN comments ON (posts.id = comments.post_id) WHERE posts.id = ?';
                 $this->sql($sql, [$id]);
@@ -120,6 +156,10 @@
             header('Location: ../public/index.php?action=getChaptersAndReportedComments');
 		}
 
+        /**
+         * @param array $row
+         * @return Post
+         */
 		private function buildObject(array $row){
 		    $post = new Post();
 		    $post->setId($row['id']);
