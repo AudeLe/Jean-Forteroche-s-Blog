fe3440927c80fd7170798582a00d33ccf4d241f2
AudeL <aude.leissen@gmail.com>
2018-04-28 04:37:54 +0200

    Adding the Bootstrap size without the responsive for now.

--- template/editCommentView.php
+++ template/editCommentView.php
@@ -1,26 +1,24 @@
-<?php $title = htmlspecialchars('Edition de commentaire'); ?>
+<?php $this->title = 'Edition de commentaire'; ?>
 
-<?php ob_start(); ?>
 	
 	<h2>Commentaires</h2>
+    <div class="row">
+        <div class="col-md-12">
+            <form action="../public/index.php?action=editedComment&id=<?= $comment->getId(); ?>&memberLogin=<?= $comment->getAuthor(); ?>" method="post">
+                <div>
+                    <input type="hidden" value="<?= $comment->getPostId(); ?>" />
+                </div>
+                <div>
+                    <p>Auteur : <?= strip_tags($comment->getAuthor()); ?></p>
+                </div>
+                <div>
+                    <label for="newComment">Commentaire</label><br />
+                    <textarea id="newComment" name="newComment"><?= strip_tags($comment->getComment()); ?></textarea>
+                </div>
+                <div>
+                    <input type="submit" value="Editer le commentaire" />
+                </div>
+            </form>
+        </div>
+    </div>
 
-	<?php
-		$comment = $editedComment->fetch();
-	?>
-
-	<form action="../public/index.php?action=editedComment&id=<?= $comment['id'] ?>" method="post">
-		<div>
-			<p>Auteur : <?= $comment['author'] ?></p>
-		</div>
-		<div>
-			<label for="newComment">Commentaire</label><br />
-			<textarea id="newComment" name="newComment"><?= $comment['comment'] ?></textarea>
-		</div>
-		<div>
-			<input type="submit" value="Editer le commentaire" />
-		</div>
-	</form>
-
-<?php $content = ob_get_clean(); ?>
-
-<?php require('template.php'); ?>
