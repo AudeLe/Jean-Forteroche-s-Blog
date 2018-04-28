2869d9f6c2a5d254033d39abcd9c25cc43b144db
AudeL <aude.leissen@gmail.com>
2018-04-28 04:41:45 +0200

    Adding the Bootstrap size without the responsive for now.

--- template/editInformations.php
+++ template/editInformations.php
@@ -1,47 +1,41 @@
-<?php $title = htmlspecialchars('Modification d\'identifiant'); ?>
+<?php $this->title = 'Modification d\'identifiant'; ?>
 
-<?php ob_start(); ?>
-
-<?php
-    $idVisitor = $visitorInformations['id'];
-?>
 
 <h3>Changer son pseudo ou son mot de passe</h3>
 
-<p>Changer son pseudonyme</p>
-<form action="../public/index.php?action=editLogin" method="post">
-    <div>
-        <input type="text" id="idVisitor" name="idVisitor" value="<?= $idVisitor ?>" />
-    </div>
-    <div>
-        <label for="editLogin">Nouveau pseudonyme</label><br />
-        <input type="text" id="editLogin" name="editLogin" />
-    </div>
-    <div>
-        <input type="submit" value="Modifier mon pseudo" />
-    </div>
-</form>
+        <p id="textChangeCredentials">Une fois votre pseudonyme ou votre mot de passe modifié, vous serez déconnecté(e) afin d'enregistrer vos modifications.<br/>
+            Veuillez vous connecter de nouveau afin d'avoir accès à votre espace personnel.
+        </p>
 
-<p>Changer son mot de passe</p>
-<p>Veuillez entrer votre nouveau mot de passe</p>
-<form action="../public/index.php?action=editPassword" method="post">
 
-    <div>
-        <input type="text" id="idVisitor" name="idVisitor" value="<?= $idVisitor ?>" />
+<div class="row">
+    <div class="col-md-6 changeCredentials">
+        <h4>Changer son pseudonyme</h4>
+        <form action="../public/index.php?action=editLogin" method="post">
+            <div>
+                <label for="editLogin">Nouveau pseudonyme</label><br />
+                <input type="text" id="editLogin" name="editLogin" />
+            </div>
+            <div>
+                <input type="submit" value="Modifier mon pseudo" class="submitButton" />
+            </div>
+        </form>
     </div>
-    <div>
-        <label for="editPassword">Mot de passe</label><br />
-        <input type="password" id="editPassword" name="editPassword" />
-    </div>
-    <div>
-        <label for="editPasswordCheck">Veuillez entrer de nouveau votre mot de passe</label><br />
-        <input type="password" id="editPasswordCheck" name="editPasswordCheck" />
-    </div>
-    <div>
-        <input type="submit" value="Modifier mon mot de passe" />
-    </div>
-</form>
 
-<?php $content = ob_get_clean(); ?>
-
-<?php require('template.php'); ?>
+    <div class="col-md-6 changeCredentials">
+        <h4>Changer son mot de passe</h4>
+        <form action="../public/index.php?action=editPassword" method="post">
+            <div>
+                <label for="editPassword">Nouveau mot de passe</label><br />
+                <input type="password" id="editPassword" name="editPassword" />
+            </div>
+            <div>
+                <label for="editPasswordCheck">Veuillez entrer de nouveau votre mot de passe</label><br />
+                <input type="password" id="editPasswordCheck" name="editPasswordCheck" />
+            </div>
+            <div>
+                <input type="submit" value="Modifier mon mot de passe" class="submitButton" />
+            </div>
+        </form>
+    </div>
+</div>
