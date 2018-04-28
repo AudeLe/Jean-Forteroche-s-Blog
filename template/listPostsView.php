ce6c4764e11434721329177c013d211e4b054ab2
AudeL <aude.leissen@gmail.com>
2018-04-28 04:40:33 +0200

    Adding the Bootstrap size without the responsive for now.

--- template/listPostsView.php
+++ template/listPostsView.php
@@ -1,34 +1,49 @@
 <?php $this->title = 'Accueil'; ?>
 
-	<h1>Bienvenue sur mon blog</h1>
+
+    <h1>Bienvenue sur mon blog</h1>
 
     <p>
         Je souhaite vous faire partager mon ouvrage : "Billet simple pour l'Alaska".
         Les chapitres seront publiés au fur et à mesure de leur écriture. N'hésitez pas à me faire part de vos remarques et de vos corrections.
     </p>
 
+    <p>Derniers chapitres :</p>
 
-    <div id="authorPresentation">
-        <h4>Jean Forteroche</h4>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam mauris tortor, euismod a augue in, lacinia semper ante. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse sed quam tempus risus rutrum ullamcorper quis in eros. Nulla neque dui, eleifend ultricies dolor eu, molestie accumsan nisl. Fusce congue risus et turpis finibus fermentum ut gravida tortor. Nunc ut consectetur nunc, ut lobortis mi. Mauris venenatis vulputate turpis quis sagittis. Vestibulum lacinia ex dolor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. </p>
+    <div class="row">
+        <div id="chaptersPresentation" class="col-md-9">
+
+            <?php
+
+            foreach($posts[0] as $post):
+                ?>
+                <div class="news">
+                    <h3><a href="../public/index.php?action=post&id=<?= $post->getId(); ?>"><?= $post->getTitle(); ?></a></h3>
+                    <p class="date datePosts">Créé le <em><?= $post->getCreationDate(); ?></em></p>
+                    <p class="postContent"><?= $post->getContent(); ?>
+                        <span class="ellipsis">&#133;</span>
+                        <span class="fill"></span>
+                    </p>
+
+                </div>
+                <br />
+
+            <?php endforeach; ?>
+
+        </div>
+
+        <div id="authorPresentation" class="col-md-3">
+            <h3>Jean Forteroche</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam mauris tortor, euismod a augue in, lacinia semper ante.<br/>
+                Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse sed quam tempus risus rutrum ullamcorper quis in eros. Nulla neque dui, eleifend ultricies dolor eu, molestie accumsan nisl. Fusce congue risus et turpis finibus fermentum ut gravida tortor.<br />
+                Nunc ut consectetur nunc, ut lobortis mi. Mauris venenatis vulputate turpis quis sagittis. Vestibulum lacinia ex dolor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+            </p>
+        </div>
     </div>
 
-    <p>Derniers chapitres :</p>
-	<?php
-
-        foreach($posts[0] as $post):
-            ?>
-            <div class="news">
-                <h3><a href="../public/index.php?action=post&id=<?= $post->getId(); ?>"><?= $post->getTitle(); ?></a></h3>
-                <p><?= $post->getCreationDate(); ?></p>
-                <p><?= $post->getContent(); ?></p>
-
-            </div>
-            <br />
-
-        <?php endforeach; ?>
-
+    <div class="pages">
         <?php foreach($posts[1] as $page): ?>
-         <span><?= $page ?></span>
+            <span><?= $page; ?></span>
 
         <?php endforeach;?>
+    </div>
