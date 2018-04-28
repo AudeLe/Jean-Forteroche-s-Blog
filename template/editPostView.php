fca18e7951a0e771cfef06dde8b181fea59bf7db
AudeL <aude.leissen@gmail.com>
2018-04-28 04:43:07 +0200

    Adding the Bootstrap size without the responsive for now.

--- template/editPostView.php
+++ template/editPostView.php
@@ -2,18 +2,20 @@
 
 	<h2>Edition de chapitre</h2>
 
-
-	<form action="../public/index.php?action=editedPost&id=<?= $post->getId(); ?>" method="post">
-		<div>
-			<label for="newTitle">Titre du post</label><br />
-			<textarea id="newTitle" name="newTitle"><?= $post->getTitle(); ?></textarea>
-		</div>
-		<div>
-			<label for="newPost">Post</label><br />
-			<textarea id="newPost" name="newPost"><?= $post->getContent(); ?></textarea>
-		</div>
-		<div>
-			<input type="submit" value="Editer le post" />
-		</div>
-		
-	</form>
+    <div class="row">
+        <div class="col-md-12">
+            <form action="../public/index.php?action=editedPost&id=<?= $post->getId(); ?>" method="post">
+                <div>
+                    <label for="newTitle">Titre du post</label><br />
+                    <input type="text" id="newTitle" name="newTitle" value="<?= $post->getTitle(); ?>" />
+                </div>
+                <div>
+                    <label for="newPost">Post</label><br />
+                    <textarea id="newPost" name="newPost"><?= $post->getContent(); ?></textarea>
+                </div>
+                <div>
+                    <input type="submit" value="Editer le post" class="submitButton" />
+                </div>
+            </form>
+        </div>
+    </div>
