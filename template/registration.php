5bcff47e2056fb3b446ae291f02f9e0f8fa61a1e
AudeL <aude.leissen@gmail.com>
2018-04-28 04:48:44 +0200

    Adding the Bootstrap size without the responsive for now.

--- template/registration.php
+++ template/registration.php
@@ -1,9 +1,12 @@
 <?php header('refresh:5; url=../public/index.php'); ?>
 
-<?php $this->title = htmlspecialchars('Inscription'); ?>
+<?php $this->title = 'Inscription'; ?>
 
-
-<p>
-Vous allez être redirigé vers la page d'accueil du blog.<br />
-    Si ce n'est pas le cas, veuillez cliquer sur ce <a href="../public/index.php">lien</a>.
-</p>
+<div class="row">
+    <div class="col-md-12">
+        <p>
+        Vous allez être redirigé vers la page d'accueil du blog.<br />
+        Si ce n'est pas le cas, veuillez cliquer sur ce <a href="../public/index.php">lien</a>.
+        </p>
+    </div>
+</div>
