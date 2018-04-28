16aa1497cd64154dff8d9faca04c7b26bc512ea8
AudeL <aude.leissen@gmail.com>
2018-04-28 04:39:15 +0200

    Adding the Bootstrap size without the responsive for now. Adding also the admin's comments with the same functions then for other members.

--- template/adminView.php
+++ template/adminView.php
@@ -1,13 +1,11 @@
 <?php $this->title = 'Page d\'administration'; ?>
 
 
-    <h1>Page d'administration</h1>
+<!-- Add a chapter -->
+<h2>Ajout d'article</h2>
+    <div class="container-fluid row" id="addChapter">
 
-    <!-- Add a chapter -->
-    <div>
-        <h2>Ajout d'article</h2>
-
-        <form action="../public/index.php?action=addPost" method="post">
+        <form action="../public/index.php?action=addPost" method="post" class="col-md-12">
             <div>
                 <label for="title">Titre du chapitre</label><br />
                 <input type="text" id="title" name="title">
@@ -17,101 +15,150 @@
                 <textarea id="article" name="article"></textarea>
             </div>
             <div>
-                <input type="submit" value="Ajouter cet article">
+                <input type="submit" value="Ajouter cet article" class="submitButton">
+            </div>
+        </form>
+
+    </div>
+
+    <div class="row">
+        <div class="col-md-12">
+            <ul class="nav nav-tabs" id="myTab" role="tablist">
+                <li class="nav-item">
+                    <a class="nav-link active" id="myPosts-tab" data-toggle="tab" href="#myPosts" role="tab" aria-controls="myPosts" aria-selected="true">Mes chapitres</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" id="reportedCommentsArea-tab" data-toggle="tab" href="#reportedCommentsArea" role="tab" aria-controls="reportedCommentsArea" aria-selected="false">Commentaires signalés</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" id="account-tab" data-toggle="tab" href="#account" role="tab" aria-controls="account" aria-selected="false">Compte</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" id="myComments-tab" data-toggle="tab" href="#myComments" role="tab" aria-controls="myComments" aria-selected="true">Mes commentaires</a>
+                </li>
+            </ul>
+        </div>
+
+        <!-- Display all the posts/chapters existing for the admin to edit and/or delete -->
+        <div class="tab-content col-md-12" id="myTabContent">
+            <div class="tab-pane fade show active" id="myPosts" role="tabpanel" aria-labelledby="myPosts-tab">
+                <table class="col-md-12">
+                    <tr class="listPostsAdmin">
+                        <th class="col-md-2 postsAdmin">Titre du chapitre</th>
+                        <th class="col-md-6 postsAdmin">Chapitre</th>
+                        <th class="col-md-2 postsAdmin">Date de publication</th>
+                        <th class="col-md-1 postsAdmin">Modifer l'article</th>
+                        <th class="col-md-1 postsAdmin">Supprimer l'article</th>
+                    </tr>
+
+                    <?php
+
+                    foreach($posts as $post):
+                        ?>
+                        <tr class="listPostsAdmin">
+                            <td class="col-md-2 postsAdmin"><?= $post->getTitle(); ?></td>
+                            <td class="col-md-6 postsAdmin">
+                                <p class="postContent"><?= $post->getContent(); ?>
+                                <span class="ellipsis">&#133;</span>
+                                <span class="fill"></span>
+                                </p>
+                            </td>
+                            <td class="col-md-2 postsAdmin"><?= $post->getCreationDate(); ?></td>
+                            <td class="col-md-1 postsAdmin"><a href="../public/index.php?action=editPost&id=<?= $post->getId(); ?>"><i class="fas fa-edit"></i></a></td>
+                            <td class="col-md-1 postsAdmin"><a href="../public/index.php?action=deletePost&id=<?= $post->getId(); ?>"><i class="fas fa-trash alt"></i></a></td>
+                        </tr>
+                    <?php endforeach; ?>
+                </table>
+
+
             </div>
 
-        </form>
+
+            <!-- Display all the reported comments for the admin to moderate -->
+
+            <div class="tab-pane fade col-md-12" id="reportedCommentsArea" role="tabpanel" aria-labelledby="reportedCommentsArea-tab">
+                <div class="row">
+                    <table class="col-md-12">
+                        <tr class="reportedComments">
+                            <th class="col-md-2 reportedComment">Chapitre</th>
+                            <th class="col-md-2 reportedComment">Auteur</th>
+                            <th class="col-md-5 reportedComment">Commentaire</th>
+                            <th class="col-md-1 reportedComment">Date du commentaire</th>
+                            <th class="col-md-1 reportedComment">Ignorer le commentaire</th>
+                            <th class="col-md-1 reportedComment">Supprimer le commentaire</th>
+                        </tr>
+
+                        <?php
+                        foreach($reportedComments as $reportedComment):
+                            ?>
+                            <tr class="reportedComments">
+
+                                <td class="col-md-2 reportedComment"><?= $reportedComment->getPostId()->getTitle(); ?></td>
+                                <td class="col-md-2 reportedComment"><?= $reportedComment->getAuthor(); ?></td>
+                                <td class="col-md-5 reportedComment"><?= $reportedComment->getComment(); ?></td>
+                                <td class="col-md-1 reportedComment"><?= $reportedComment->getCommentDate(); ?></td>
+                                <td class="col-md-1 reportedComment"><a href="../public/index.php?action=ignoreReportedComment&id=<?= $reportedComment->getId(); ?>"><i class="fas fa-thumbs-up"></i></a></td>
+                                <td class="col-md-1 reportedComment"><a href="../public/index.php?action=deleteComment&id=<?= $reportedComment->getId(); ?>"><i class="fas fa-trash-alt"></i></a></td>
+                            </tr>
+
+                        <?php endforeach; ?>
+                    </table>
+                </div>
+            </div>
+
+
+            <!-- Elements allowing to change the credentials of the account -->
+            <div class="tab-pane fade col-md-12" id="account" role="tabpanel" aria-labelledby="account-tab">
+                <div class="row">
+                    <div class="col-md-12 changeCredentials">
+                        <h4>Changer son pseudo ou son mot de passe</h4>
+                        <p>Veuillez entrer vos identifiants actuels</p>
+                        <form action="../public/index.php?action=checkInformations" method="post">
+                            <div>
+                                <label for="checkLogin">Pseudo</label><br />
+                                <input type="text" id="checkLogin" name="checkLogin" value="<?= $_SESSION['login'] ?>" readonly/>
+                            </div>
+                            <div>
+                                <label for="checkPassword">Mot de passe</label><br />
+                                <input type="password" id="checkPassword" name="checkPassword" />
+                            </div>
+                            <div>
+                                <input type="submit" value="Confirmer mes identifiants" class="submitButton" />
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Display all the member's comments -->
+            <div class="tab-pane fade" id="myComments" role="tabpanel" aria-labelledby="myComments-tab">
+                <div class="row">
+                    <table class="col-md-12">
+                        <tr class="listCommentsMember">
+                            <th class="col-md-2 commentsMember">Chapitre</th>
+                            <th class="col-md-6 commentsMember">Commentaire</th>
+                            <th class="col-md-2 commentsMember">Date de publication</th>
+                            <th class="col-md-1 commentsMember">Modifer le commentaire</th>
+                            <th class="col-md-1 commentsMember">Supprimer le commentaire</th>
+                        </tr>
+
+                        <?php
+                        foreach ($comments as $comment){
+                            ?>
+                            <tr class="listCommentsMember">
+                                <td class="col-md-2 commentsMember"><?= $comment->getPostId()->getTitle(); ?></td>
+                                <td class="col-md-6 commentsMember"><?= $comment->getComment(); ?></td>
+                                <td class="col-md-2 commentsMember"><?= $comment->getCommentDate(); ?></td>
+                                <td class="col-md-1 commentsMember"><a href="../public/index.php?action=editComment&id=<?= $comment->getId(); ?>"><i class="fas fa-edit"></i></a></td>
+                                <td class="col-md-1 commentsMember"><a href="../public/index.php?action=deleteComment&id=<?= $comment->getId(); ?>"><i class="fas fa-trash alt"></i></a></td>
+                            </tr>
+                            <?php
+                        }
+                        ?>
+                    </table>
+
+                </div>
+            </div>
+
+        </div>
     </div>
-
-
-    <ul class="nav nav-tabs" id="myTab" role="tablist">
-        <li class="nav-item">
-            <a class="nav-link active" id="myPosts-tab" data-toggle="tab" href="#myPosts" role="tab" aria-controls="myPosts" aria-selected="true">Mes chapitres</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" id="reportedCommentsArea-tab" data-toggle="tab" href="#reportedCommentsArea" role="tab" aria-controls="reportedCommentsArea" aria-selected="false">Commentaires signalés</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" id="account-tab" data-toggle="tab" href="#account" role="tab" aria-controls="account" aria-selected="false">Compte</a>
-        </li>
-    </ul>
-
-    <div class="tab-content" id="myTabContent">
-        <div class="tab-pane fade show active" id="myPosts" role="tabpanel" aria-labelledby="myPosts-tab">
-            <table>
-                <tr>
-                    <th>Titre du chapitre</th>
-                    <th>Chapitre</th>
-                    <th>Date de publication</th>
-                    <th>Modifer l'article</th>
-                    <th>Supprimer l'article</th>
-                </tr>
-
-                <?php
-
-                foreach($posts as $post){
-                    ?>
-                    <tr>
-                        <td><?= $post->getTitle(); ?></td>
-                        <td><?= $post->getContent(); ?></td>
-                        <td><?= $post->getCreationDate(); ?></td>
-                        <td><a href="../public/index.php?action=editPost&id=<?= $post->getId(); ?>">Editer le post</a></td>
-                        <td><a href="../public/index.php?action=deletePost&id=<?= $post->getId(); ?>">Supprimer le post</a></td>
-                    </tr>
-                <?php
-                    }
-                    ?>
-            </table>
-
-        </div>
-
-
-
-        <div class="tab-pane fade" id="reportedCommentsArea" role="tabpanel" aria-labelledby="reportedCommentsArea-tab">
-            <table>
-                <tr>
-                    <th>Numéro du chapitre</th>
-                    <th>Auteur</th>
-                    <th>Commentaire</th>
-                    <th>Date du commentaire</th>
-                    <th>Supprimer le commentaire</th>
-                </tr>
-
-                <?php
-                foreach($comments as $comment){
-                    ?>
-                    <tr>
-
-                        <td><?= $comment->getPostId()->getTitle(); ?></td>
-                        <td><?= $comment->getAuthor(); ?></td>
-                        <td><?= $comment->getComment(); ?></td>
-                        <td><?= $comment->getCommentDate(); ?></td>
-                        <td><a href="../public/index.php?action=deleteComment&id=<?= $comment->getId(); ?>">Supprimer le post</a></td>
-                    </tr>
-                <?php
-                }
-                ?>
-            </table>
-
-        </div>
-
-        <div class="tab-pane fade" id="account" role="tabpanel" aria-labelledby="account-tab">
-            <h3>Changer son pseudo ou son mot de passe</h3>
-            <p>Veuillez entrer vos identifiants actuels</p>
-            <form action="../public/index.php?action=checkInformations" method="post">
-                <div>
-                    <label for="checkLogin">Pseudo</label><br />
-                    <input type="text" id="checkLogin" name="checkLogin" value="<?= $_SESSION['login'] ?>" readonly/>
-                </div>
-                <div>
-                    <label for="checkPassword">Mot de passe</label><br />
-                    <input type="password" id="checkPassword" name="checkPassword" />
-                </div>
-                <div>
-                    <input type="submit" value="Confirmer mes identifiants" />
-                </div>
-            </form>
-        </div>
-
-    </div>
-
