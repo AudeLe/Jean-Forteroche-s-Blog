6f822c72b70e6aa601d67d014eac3059db3683c3
AudeL <aude.leissen@gmail.com>
2018-04-28 15:34:18 +0200

    Adding the sizes for the responsive elements.

--- template/listPostsView.php
+++ template/listPostsView.php
@@ -1,24 +1,25 @@
 <?php $this->title = 'Accueil'; ?>
 
+    <div class="row" id="listPostsWelcome">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <h1>Bienvenue sur mon blog</h1>
 
-    <h1>Bienvenue sur mon blog</h1>
+            <p>
+                Je souhaite vous faire partager mon ouvrage : "Billet simple pour l'Alaska".
+                Les chapitres seront publiés au fur et à mesure de leur écriture. N'hésitez pas à me faire part de vos remarques et de vos corrections.
+            </p>
+        </div>
+    </div>
 
-    <p>
-        Je souhaite vous faire partager mon ouvrage : "Billet simple pour l'Alaska".
-        Les chapitres seront publiés au fur et à mesure de leur écriture. N'hésitez pas à me faire part de vos remarques et de vos corrections.
-    </p>
-
-    <p>Derniers chapitres :</p>
 
     <div class="row">
-        <div id="chaptersPresentation" class="col-md-9">
-
+        <div class="col-lg-9 col-md-9 col-sm-9 col-xs-12">
             <?php
 
             foreach($posts[0] as $post):
                 ?>
                 <div class="news">
-                    <h3><a href="../public/index.php?action=post&id=<?= $post->getId(); ?>"><?= $post->getTitle(); ?></a></h3>
+                    <h4><a href="../public/index.php?action=post&id=<?= $post->getId(); ?>"><?= $post->getTitle(); ?></a></h4>
                     <p class="date datePosts">Créé le <em><?= $post->getCreationDate(); ?></em></p>
                     <div class="postContent"><?= $post->getContent(); ?>
                         <span class="ellipsis">&#133;</span>
@@ -32,7 +33,7 @@
 
         </div>
 
-        <div id="authorPresentation" class="col-md-3">
+        <div id="authorPresentation" class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
             <h3>Jean Forteroche</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam mauris tortor, euismod a augue in, lacinia semper ante.<br/>
                 Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse sed quam tempus risus rutrum ullamcorper quis in eros. Nulla neque dui, eleifend ultricies dolor eu, molestie accumsan nisl. Fusce congue risus et turpis finibus fermentum ut gravida tortor.<br />
