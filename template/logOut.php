3d43b428bd2de716ec75df729533eb53712ef308
AudeL <aude.leissen@gmail.com>
2018-04-28 04:44:48 +0200

    Adding the Bootstrap size without the responsive for now.

--- template/logOut.php
+++ template/logOut.php
@@ -1,8 +1,6 @@
 <?php header('refresh:5; url=../public/index.php'); ?>
 
-<?php $this->title = htmlspecialchars('Déconnexion'); ?>
+<?php $this->title = 'Déconnexion'; ?>
 
-
-
-    <p>Vous venez d'être déconnecté avec succès.</p>
-
+<p>Vous venez d'être déconnecté avec succès.</p>
+<p>Vous allez être redirigé dans quelques instants vers la page d'accueil.</p>
